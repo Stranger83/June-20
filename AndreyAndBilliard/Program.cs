6b44b045cd020c6ba38b5cbf90dcc9ed16d9334b
Stranger <jmanova83@gmail.com>
2017-06-22 22:29:26 +0300

    08/10 solved

---
+++ AndreyAndBilliard/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreyAndBilliard
+{
+	class Program
+	{
+		class Customer
+		{
+			public string Name { get; set; }
+			public Dictionary<string, int> BoughtItems { get; set; }
+			public decimal Bill { get; set; }
+		}
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			Dictionary<string, decimal> products = new Dictionary<string, decimal>();
+			for (int i = 0; i < n; i++)
+			{
+				var input = Console.ReadLine().Split('-').ToArray();
+				if (!products.ContainsKey(input[0]))
+				{
+					products[input[0]] = 0;
+				}
+				products[input[0]] = decimal.Parse(input[1]);
+			}
+			List<Customer> customers = new List<Customer>();
+			while (true)
+			{
+				var input = Console.ReadLine();
+				if (input == "end of clients")
+				{
+					break;
+				}
+				string[] tokens = input.Split('-').ToArray();
+				string[] buyTokens = tokens[1].Split(',');
+				Customer cust = new Customer {Name = tokens[0],
+					BoughtItems = new Dictionary<string, int>()};
+				if (!products.ContainsKey(buyTokens[0]))
+				{
+					continue;
+				} else if (!cust.BoughtItems.ContainsKey(buyTokens[0]))
+				{
+					cust.BoughtItems[buyTokens[0]] = int.Parse(buyTokens[1]);
+				}
+				else if (cust.BoughtItems.ContainsKey(buyTokens[0]) && products.ContainsKey(buyTokens[0]))
+				{
+					cust.BoughtItems[buyTokens[0]] += int.Parse(buyTokens[1]);
+				}
+				foreach (var kvp in cust.BoughtItems)
+				{
+					cust.Bill = kvp.Value * products[kvp.Key];
+				}
+				customers.Add(cust);
+			}
+			var ordered = customers.OrderBy(x => x.Name);
+
+			decimal totalBill = 0;
+			foreach (var customer in ordered)
+			{
+				Console.WriteLine(customer.Name);
+				foreach (var kvp in customer.BoughtItems)
+				{
+					Console.WriteLine($"-- {kvp.Key} - {kvp.Value}");
+				}
+				Console.WriteLine($"Bill: {customer.Bill:f2}");
+				totalBill += customer.Bill;
+			}
+			
+			Console.WriteLine($"Total bill: {totalBill:f2}");
+		}
+	}
+}
---
+++ MentorGroup/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorGroup
+{
+	class Program
+	{
+		class User
+		{
+			public string Name { get; set; }
+			public List<DateTime> Dates { get; set; }
+			public List<string> Comments { get; set; }
+		}
+		static void Main(string[] args)
+		{
+			List<User> allUsers = new List<User>();
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "end of dates")
+				{
+					break;
+				}
+				string[] nameDates = input.Split(' ', ',');
+				string name = nameDates[0];
+				var textDates = nameDates.Skip(1).ToList();
+				List<DateTime> dates = textDates
+					.Select(d => DateTime
+					.ParseExact(d, "dd/MM/yyyy", CultureInfo.InvariantCulture))
+					.ToList();
+				if (allUsers.Select(u => u.Name).Contains(name))
+				{
+					for (int i = 0; i < allUsers.Count; i++)
+					{
+						if (allUsers[i].Name == name)
+						{
+							allUsers[i].Dates.AddRange(dates);
+						}
+					}
+					
+				}
+				else
+				{
+					User user = new User
+					{
+						Name = name,
+						Dates = dates,
+						Comments = new List<string>()
+					};
+					allUsers.Add(user);
+				}
+				
+			}
+
+			while (true)
+			{
+				var input = Console.ReadLine();
+				if (input == "end of comments")
+				{
+					break;
+				}
+				var nameComment = input.Split('-');
+				var name = nameComment[0];
+				var comment = nameComment[1];
+				foreach (var user in allUsers)
+				{
+					if (user.Name == name)
+					{
+						user.Comments.Add(comment);
+					}
+				}
+			}
+
+			allUsers = allUsers.OrderBy(u => u.Name).ToList();
+			foreach (var user in allUsers)
+			{
+				Console.WriteLine(user.Name);
+				Console.WriteLine("Comments:");
+				foreach (var comment in user.Comments)
+				{
+					Console.WriteLine($"- {comment}");
+				}
+				Console.WriteLine("Dates attended:");
+				user.Dates = user.Dates.OrderBy(x => x).ToList();
+				foreach (var date in user.Dates)
+				{
+					Console.WriteLine($"-- {date:dd/MM/yyyy}");
+				}
+			}
+		}
+	}
+}
